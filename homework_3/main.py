589f247a59763201d5da51e7c5a46c7e783ba5e1
vsrede <vsrede94@gmail.com>
2023-06-03 23:00:20 +0200

    added task_2

--- homework_3/main.py
+++ homework_3/main.py
@@ -7,9 +7,12 @@
     *********************
 """
 import csv
-from flask import Flask
+
+import requests
+from flask import Flask, Response
 from flask import request
 from faker import Faker
+
 
 # $env:FLASK_APP = "homework_3/main"
 # flask run --no-debugger
@@ -33,31 +36,39 @@ def generate_students():
         length_students_list = int(length_students_list)
 
     route_to_cvs = "homework_3/attachments/db_students.csv"
-    # dictionary_students = {}
-    # for i in range(1, length_students_list):
-    #      dictionary_students[i] = {
-    #         'fname': fake.first_name(),
-    #         'lname': fake.last_name(),
-    #         'email': fake.email(),
-    #         'pass': fake.password(length=12),
-    #         'dob': str(fake.date_of_birth(minimum_age=18, maximum_age=30))
-    #     }
-    fieldnames = ['first_name', 'last_name', 'email', 'password', 'date_of_birth']
-    with open(route_to_cvs, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+    list_students = []
+    csv_columns = ['first_name', 'last_name', 'email', 'password', 'date_of_birth']
+    for i in range(1, length_students_list):
+        student = {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+            'email': fake.email(),
+            'password': fake.password(length=12),
+            'date_of_birth': str(fake.date_of_birth(minimum_age=18, maximum_age=30))
+        }
+
+        list_students.append(student)
+
+    with open(route_to_cvs, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
         writer.writeheader()
+        writer.writerows(list_students)
+    # fieldnames = ['first_name', 'last_name', 'email', 'password', 'date_of_birth']
+    # with open(route_to_cvs, mode='w', newline='') as file:
+    #     writer = csv.DictWriter(file, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #
+    #     for i in range(1, length_students_list):
+    #         row_data = {
+    #             'first_name': fake.first_name(),
+    #             'last_name': fake.last_name(),
+    #             'email': fake.email(),
+    #             'password': fake.password(length=12),
+    #             'date_of_birth': str(fake.date_of_birth(minimum_age=18, maximum_age=30))
+    #         }
+    #         writer.writerow(row_data)
 
-        for i in range(1, length_students_list):
-            row_data = {
-                'first_name': fake.first_name(),
-                'last_name': fake.last_name(),
-                'email': fake.email(),
-                'password': fake.password(length=12),
-                'date_of_birth': str(fake.date_of_birth(minimum_age=18, maximum_age=30))
-            }
-            writer.writerow(row_data)
-
-    # df = pd.DataFrame.from_dict(dictionary_students)
+    # df = pd.DataFrame.from_dict(list_students)
     # df.to_csv(r'homework_3/attachments/db_students.csv', index=False, header=True)
 
     return csv_printer(route_to_cvs)
@@ -70,6 +81,7 @@ def csv_printer(route_to_cvs):
         for row in csv_reader:
             csv_lines.append('_____'.join(row))
     return '<br>'.join(csv_lines)
+
 
 """
     def get_bitcoin_value():
@@ -85,4 +97,17 @@ def csv_printer(route_to_cvs):
     """
 
 
+@app.route("/bitcoin_value")
+def get_bitcoin_value():
+    currency = request.args.get('currency', 'USD')
+    url = "https://bitpay.com/api/rates"
+    list_currency_btc = requests.get(url, {})
+    if list_currency_btc.status_code != 200:
+        return Response("ERROR: Something went wrong", status=list_currency_btc.status_code)
+    list_currency_btc = list_currency_btc.json()
 
+    print(type(currency))
+    print(type(list_currency_btc))
+    value_currency = next((d for d in list_currency_btc if d.get('code') == currency), None)
+
+    return f"Bitcoin exchange rate is {value_currency['rate']} {value_currency['name']}s"
